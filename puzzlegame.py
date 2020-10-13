673af62e7d7a932da4299c07aab925af64233f5f
Heikki Jylhä <hjjylh@gmail.com>
2020-10-13 23:47:07 +0300

    Started work on puzzlegame class

--- puzzlegame.py
+++ puzzlegame.py
@@ -1,4 +1,21 @@
-from puzzlegame_setup import *
-from positions import *
-import puzzlesolver as ps
+from positions import Positions
 
+class Puzzlegame:
+
+    def __init__(self):
+        # nothing active at the start
+        self.active_piece = -1
+        self.highlighted = False
+        # should this be the whole object or just the position tuple?
+        self.current_pos = Positions().pieces
+        self.pos_log = [Positions().pieces]
+        self.move_log = []
+        # solution stuff
+        self.solution_mode = False
+        self.pos_log_opt = []
+        self.move_log_opt = []
+
+    def is_solved(self):
+        if self.current_pos[-1] == (3, 1):
+            return True
+        return False
