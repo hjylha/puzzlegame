d7ea289cd5dbb17ade5799f86aac26e14e3a0d0e
Heikki Jylhä <hjjylh@gmail.com>
2022-05-12 13:56:46 +0300

    some work on overhauling db_functions

--- db_functions.py
+++ db_functions.py
@@ -9,16 +9,97 @@ DB_FILEPATH = Path(__file__).parent / DB_FILENAME
 POSITIONS_TABLE_NAME = "positions"
 
 
-def perform_db_actions(command_to_execute, *args):
+# first some functions to change data to suitable form
+# 
+# transforming (x, y) coordinates to single number (and back)
+def coord_as_int(coord):
+    return coord[0] + coord[1] * NUM_OF_ROWS
+
+def int_as_coord(num):
+    return (num % NUM_OF_ROWS, num // NUM_OF_ROWS)
+
+
+# transforming tuple of coords to a string of ints (and vice versa)
+def change_pos_tuple_to_string(pos_tuple):
+    int_coords = [coord_as_int(coord) for coord in pos_tuple]
+    return ",".join([str(num) for num in int_coords])
+
+def change_pos_string_to_tuple(pos_as_string):
+    int_coords = pos_as_string.split(",")
+    return tuple(int_as_coord(int(num)) for num in int_coords)
+
+
+# transforming a set of ints to a string of ints (and vice versa)
+def change_int_set_to_string(int_set):
+    if int_set == set():
+        return ''
+    return ",".join([str(num) for num in int_set])
+
+def change_neighbor_string_to_set(set_as_string):
+    if set_as_string == '':
+        return set()
+    return set(int(num) for num in set_as_string.split(","))
+    
+
+# construct a Positions object based on a row in positions table
+def table_row_to_position(table_row):
+    pos_coords = change_pos_string_to_tuple(table_row[0])
+    neighbors = change_neighbor_string_to_set(table_row[4])
+    return Positions(pos_coords, table_row[1], table_row[2], table_row[3], neighbors)
+
+def position_to_table_row(pos):
+    pieces_str = change_pos_tuple_to_string(pos.pieces)
+    neighbors = change_int_set_to_string(pos.neighbors)
+    return (pieces_str, pos.stepnum, pos.distance_to_end, pos.pos_id, neighbors)
+
+
+# now onto the actual db operations
+# 
+def perform_db_actions(function_to_execute, *args):
     with sqlite3.connect(DB_FILEPATH) as conn:
-        c = conn.cursor()
-        c.execute(command_to_execute)
-        # maybe there are more commands
-        if args:
-            conn.commit()
-            for command in args:
-                c.execute(command)
-                conn.commit()
+        to_return = function_to_execute(conn, *args)
+    return to_return
+
+
+def execute_commands(conn, *commands):
+    c = conn.cursor()
+    for command in commands:
+        c.execute(command)
+        conn.commit()
+    return
+
+
+# try to select stuff
+def try_select(conn):
+    c = conn.cursor()
+    command = f"SELECT * FROM {POSITIONS_TABLE_NAME}"
+    try:
+        c.execute(command)
+        return True
+    except sqlite3.OperationalError:
+        return False
+
+
+# return all the rows with ids in a given list
+def select_pos_by_id(conn, pos_id_list):
+    condition = " OR ".join(["id = ?" for _ in pos_id_list])
+    command = f"SELECT * FROM {POSITIONS_TABLE_NAME} WHERE {condition}"
+    c = conn.cursor()
+    c.execute(command, pos_id_list)
+    return c.fetchall()
+
+
+def perform_db_commands(command_to_execute, *args):
+    perform_db_actions(execute_commands, command_to_execute, *args)
+    # with sqlite3.connect(DB_FILEPATH) as conn:
+    #     c = conn.cursor()
+    #     c.execute(command_to_execute)
+    #     # maybe there are more commands
+    #     if args:
+    #         conn.commit()
+    #         for command in args:
+    #             c.execute(command)
+    #             conn.commit()
 
 
 def create_pos_db_command():
@@ -31,29 +112,38 @@ def create_pos_db_command():
     )
         """
 
+
 # Is this a good way to structure the database???
 def create_pos_db():
-    perform_db_actions(create_pos_db_command())
+    perform_db_commands(create_pos_db_command())
     print(f"Database '{DB_FILENAME}' created")
 
+
 def reset_pos_db():
-    perform_db_actions(f"DROP TABLE {POSITIONS_TABLE_NAME}", create_pos_db_command())
+    perform_db_commands(f"DROP TABLE {POSITIONS_TABLE_NAME}", create_pos_db_command())
     print(f"Database '{DB_FILENAME}' reset")
+
 
 def does_db_exist():
     if not DB_FILEPATH.exists():
         return False
-    conn = sqlite3.connect("position.db")
-    c = conn.cursor()
-    try:
-        c.execute("SELECT * FROM positions")
-        return True
-    except sqlite3.OperationalError:
-        return False
+    return perform_db_actions(try_select)
 
 def check_pos_db():
     conn = sqlite3.connect("position.db")
     c = conn.cursor()
+
+    c.execute(f"SELECT COUNT(*) FROM {POSITIONS_TABLE_NAME} WHERE dist_from end < ?", (0, ))
+    bad_count = c.fetchone()[0]
+    if bad_count:
+        return False
+    c.execute(f"SELECT MAX(dist_from_end) FROM {POSITIONS_TABLE_NAME}")
+    max_dist_to_end = c.fetchone()[0]
+    for distance in range(max_dist_to_end + 1):
+        c.execute(f"SELECT COUNT(*) FROM {POSITIONS_TABLE_NAME} WHERE dist_from_end = ?", (distance,))
+        count = c.fetchone()[0]
+        if count == 0:
+            return False
     try:
         c.execute("SELECT * FROM positions WHERE dist_from_end > -1")
         test_list = c.fetchall()
@@ -77,7 +167,7 @@ def check_pos_db():
 
 
 def save_pos_list_to_db(pos_list):
-    conn = sqlite3.connect("position.db")
+    conn = sqlite3.connect(DB_FILEPATH)
     c = conn.cursor()
     for pos in pos_list:
         pieces = change_pos_tuple_to_string(pos.pieces)
@@ -88,9 +178,9 @@ def save_pos_list_to_db(pos_list):
     conn.close()
 
 def load_pos_list_from_db():
-    conn = sqlite3.connect("position.db")
+    conn = sqlite3.connect(DB_FILEPATH)
     c = conn.cursor()
-    c.execute("SELECT * FROM positions")
+    c.execute(f"SELECT * FROM {POSITIONS_TABLE_NAME}")
     pos_list_raw = c.fetchall()
     conn.close()
     # format the pos list
@@ -104,29 +194,13 @@ def load_pos_list_from_db():
         pos_list.append(pos)
     return pos_list
 
-
-def coord_as_int(coord):
-    return coord[0] + coord[1] * NUM_OF_ROWS
-
-def int_as_coord(num):
-    return (num % NUM_OF_ROWS, num // NUM_OF_ROWS)
-
-
-def change_pos_tuple_to_string(pos_tuple):
-    int_coords = [coord_as_int(coord) for coord in pos_tuple]
-    return ",".join([str(num) for num in int_coords])
-
-
-# how to get info from position string to position tuple
-def change_pos_string_to_tuple(pos_as_string):
-    int_coords = pos_as_string.split(",")
-    return tuple(int_as_coord(int(num)) for num in int_coords)
-
-
-def change_int_set_to_string(int_set):
-    return ",".join([str(num) for num in int_set])
-
-
-def change_neighbor_string_to_set(set_as_string):
-    return set(int(num) for num in set_as_string.split(","))
+def get_pos_by_id(pos_id):
+    # command = f"SELECT * FROM {POSITIONS_TABLE_NAME} WHERE id = ?"
+    data_row = perform_db_actions(select_pos_by_id, [pos_id])[0]
+    return table_row_to_position(data_row)
     
+
+def get_pos_by_ids(pos_ids):
+    # command = f"SELECT * FROM {POSITIONS_TABLE_NAME} WHERE id = ?"
+    data_rows = perform_db_actions(select_pos_by_id, list(pos_ids))
+    return [table_row_to_position(row) for row in data_rows]
--- tests/test_db_functions.py
+++ tests/test_db_functions.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from random import randint
 
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
@@ -25,6 +26,15 @@ def monkeymodule():
     mp.undo()
 
 
+@pytest.fixture(scope='module')
+def pos_list0():
+    pos_list = [dbf.Positions()]
+    for move in range(4* dbf.Positions.PIECE_NUM):
+        if pos_list[0].move_ok(move):
+            pos_list.append(pos_list[0].make_move(move))
+            pos_list[-1].pos_id = len(pos_list) - 1
+    return pos_list
+
 
 
 # @pytest.fixture(scope='module')
@@ -38,6 +48,17 @@ class TestDB:
     def test_db_setup(self, monkeymodule, db_path):
         monkeymodule.setattr('db_functions.DB_FILEPATH', db_path)
         assert dbf.DB_FILEPATH.parent == Path(__file__).parent
+    
+    def test_does_db_exist(self, db_path):
+        # first the file does not exist
+        assert not dbf.does_db_exist()
+        # if the file exists, but is not db
+        with open(db_path, 'w') as file:
+            file.write('')
+        assert db_path.exists()
+        assert not dbf.does_db_exist()
+        # remove the file for now
+        db_path.unlink()
 
     def test_create_pos_db(self, db_path):
         assert not db_path.exists()
@@ -46,10 +67,58 @@ class TestDB:
             c = conn.cursor()
             c.execute(f'SELECT * FROM {dbf.POSITIONS_TABLE_NAME}')
             stuff = c.fetchall()
-        assert stuff == []
+        assert stuff is not None
 
-    def test_reset_pos_db(self, db_path):
+    def test_reset_pos_db(self):
+        with dbf.sqlite3.connect(dbf.DB_FILEPATH) as conn:
+            c = conn.cursor()
+            c.execute(f'INSERT INTO {dbf.POSITIONS_TABLE_NAME} VALUES (?, ?, ?, ?, ?)', ('abc', 0, 0, 0, '1,2,3'))
+            conn.commit()
+            c.execute(f'SELECT * FROM {dbf.POSITIONS_TABLE_NAME}')
+            stuff = c.fetchall()
+        assert stuff
         dbf.reset_pos_db()
+
+        with dbf.sqlite3.connect(dbf.DB_FILEPATH) as conn:
+            c = conn.cursor()
+            c.execute(f'SELECT * FROM {dbf.POSITIONS_TABLE_NAME}')
+            stuff = c.fetchall()
+        assert not stuff
+
+    def test_save_pos_list_to_db(self, pos_list0):
+        dbf.save_pos_list_to_db(pos_list0)
+
+        with dbf.sqlite3.connect(dbf.DB_FILEPATH) as conn:
+            c = conn.cursor()
+            c.execute(f'SELECT COUNT(*) FROM {dbf.POSITIONS_TABLE_NAME}')
+            assert c.fetchone()[0] > 1
+        
+    def test_load_pos_list_from_db(self, pos_list0):
+        pos_list = dbf.load_pos_list_from_db()
+
+        for pos, pos0 in zip(pos_list, pos_list0):
+            assert pos == pos0
+            assert pos.pieces == pos0.pieces
+            assert pos.stepnum == pos0.stepnum
+            assert pos.distance_to_end == pos0.distance_to_end
+            assert pos.pos_id == pos0.pos_id
+            assert pos.neighbors == pos0.neighbors
+
+    def test_get_pos_by_id(self, pos_list0):
+        id_to_test = 0
+        pos = dbf.get_pos_by_id(id_to_test)
+        assert pos == pos_list0[0]
+
+    def test_get_pos_by_ids(self, pos_list0):
+        ids_to_test = {0, 1, 2}
+        pos_list = dbf.get_pos_by_ids(ids_to_test)
+        for pos, pos0 in zip(pos_list, pos_list0):
+            assert pos == pos0
+            assert pos.pieces == pos0.pieces
+            assert pos.stepnum == pos0.stepnum
+            assert pos.distance_to_end == pos0.distance_to_end
+            assert pos.pos_id == pos0.pos_id
+            assert pos.neighbors == pos0.neighbors
 
 
 class TestTransforms:
@@ -97,7 +166,8 @@ class TestTransforms:
     @pytest.mark.parametrize(
         ('int_set', 'set_str'), [
             ({1, 2, 3, 4}, '1,2,3,4'),
-            ({-1, 52, 156, 13011}, '-1,52,156,13011')
+            ({-1, 52, 156, 13011}, '-1,52,156,13011'),
+            (set(), '')
         ]
     )
     def test_change_neighbor_string_to_set(self, int_set, set_str):
@@ -107,6 +177,7 @@ class TestTransforms:
         'int_set', [
             {1, 2, 3, 4},
             {-1, 52, 156, 13011},
+            set()
         ]
     )
     def test_change_int_set_to_string(self, int_set):
@@ -115,4 +186,30 @@ class TestTransforms:
         # assert dbf.change_int_set_to_string(int_set) == set_str
         assert dbf.change_neighbor_string_to_set(int_set_as_str) == int_set
 
-    
+    @pytest.mark.parametrize(
+        ('stepnum', 'dist_to_end', 'pos_id', 'neighbors'), [
+            (randint(-1, 64000), randint(-1, 64000), randint(-1, 64000), (randint(-1, 64000), randint(-1, 64000), randint(-1, 64000))),
+            (randint(-1, 64000), randint(-1, 64000), randint(-1, 64000), (randint(-1, 64000), randint(-1, 64000), randint(-1, 64000))),
+            (randint(-1, 64000), randint(-1, 64000), randint(-1, 64000), (randint(-1, 64000), randint(-1, 64000), randint(-1, 64000)))
+        ]
+    )
+    def test_table_row_to_position(self, stepnum, dist_to_end, pos_id, neighbors):
+        pieces_str = '4,8,13,19,0,2,15,17,7,5'
+        # stepnum = randint(-1, 64000)
+        # dist_to_end = randint(-1, 64000)
+        # pos_id = randint(-1, 64000)
+        # neighbor_str = '-1,52,156,13011'
+        neighbor_str = dbf.change_int_set_to_string(neighbors)
+        row = (pieces_str, stepnum, dist_to_end, pos_id, neighbor_str)
+        pos = dbf.table_row_to_position(row)
+        assert pos.pieces == dbf.Positions().pieces
+        assert pos.stepnum == stepnum
+        assert pos.distance_to_end == dist_to_end
+        assert pos.pos_id == pos_id
+        assert pos.neighbors == set(neighbors)
+
+    def test_position_to_table_row(self):
+        pos = dbf.Positions()
+        expected_row = ('4,8,13,19,0,2,15,17,7,5', 0, -1, 0, '')
+        assert dbf.position_to_table_row(pos) == expected_row
+
